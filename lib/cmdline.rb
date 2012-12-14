4e69ef3ebb75bba46a967a458ba5d50bf582e9b9
Marcus Barczak <mbarczak@etsy.com>
2012-12-14 16:24:38 +0000

    set defaults in cmdline class

--- lib/cmdline.rb
+++ lib/cmdline.rb
@@ -10,10 +10,12 @@ class CmdLine
                 @config[:nic] = nic
             end
 
+            @config[:discard_thresh] = 0
             opt.on '-d', '--discard=THRESH', Float, 'Discard keys with request/sec rate below THRESH' do |discard_thresh|
                 @config[:discard_thresh] = discard_thresh
             end
 
+            @config[:refresh_rate] = 500
             opt.on '-r', '--refresh=MS', Float, 'Refresh the stats display every MS milliseconds' do |refresh_rate|
                 @config[:refresh_rate] = refresh_rate
             end
